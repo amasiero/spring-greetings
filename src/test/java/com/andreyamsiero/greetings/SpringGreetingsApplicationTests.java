3e261dc62e88fdf5566f18bd70328d28bd92e438
Andrey Masiero <andreymasiero@gmail.com>
2023-02-19 13:49:29 +0000

    feat: Spring Context Test
    Helps to understand how the application context works using spring. It
    is possible to compare process creating the bean with the
    ApplicationContext object and also with the Dependency Injection process
    with Autowired annotaion.

--- src/test/java/com/andreyamsiero/greetings/SpringGreetingsApplicationTests.java
+++ src/test/java/com/andreyamsiero/greetings/SpringGreetingsApplicationTests.java
@@ -1,10 +1,30 @@
 package com.andreyamsiero.greetings;
 
+import com.andreyamsiero.greetings.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringGreetingsApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void getControllerFromAutowired() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void getControllerFromContext() {
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {

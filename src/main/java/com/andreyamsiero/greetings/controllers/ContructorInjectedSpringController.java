4ed8bf8451a4577f21eb880cfaf67e8fb8d6a0d0
Andrey Masiero <andreymasiero@gmail.com>
2023-02-20 09:50:47 +0000

    feat: using Qualifiers
    Qualifiers is an annotation which allows you to decide which bean
    implemantion we will use to inject. Qualifiers receive a string
    parameter that represents the object of the concrete class or a name
    defined into the Service annotation that you provide.

--- src/main/java/com/andreyamsiero/greetings/controllers/ContructorInjectedSpringController.java
+++ src/main/java/com/andreyamsiero/greetings/controllers/ContructorInjectedSpringController.java
@@ -2,11 +2,13 @@ package com.andreyamsiero.greetings.controllers;
 
 import com.andreyamsiero.greetings.services.GreetingsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ContructorInjectedSpringController {
 
+    @Qualifier("greetingServiceImpl")
     private final GreetingsService greetingsService;
 
     @Autowired
